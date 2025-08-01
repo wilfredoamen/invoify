19e1c90eed02b7e693ae50e72cd7df63429aa7a9
wilfred <wilfredoamen@gmail.com>
2025-08-01 16:42:14 +0100

    Updated terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,33 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "invoify" {
-  ami                    = "ami-0c94855ba95c71c99" # Ubuntu 20.04
-  instance_type          = var.instance_type
-  key_name               = var.key_name
+resource "aws_security_group" "invoify_sg" {
+  name        = "invoify-sg"
+  description = "Allow SSH and HTTP"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
+resource "aws_instance" "invoify" {
+  ami                         = "ami-0c94855ba95c71c99"
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.invoify_sg.id]
 
@@ -24,35 +46,5 @@ resource "aws_instance" "invoify" {
       private_key = file(var.private_key_path)
       host        = self.public_ip
     }
-  }
-
-  tags = {
-    Name = "InvoifyServer"
-  }
-}
-
-resource "aws_security_group" "invoify_sg" {
-  name        = "invoify-sg"
-  description = "Allow HTTP and SSH"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
